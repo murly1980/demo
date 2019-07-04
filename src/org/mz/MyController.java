d77ace1b09a71f2c88632ea6bc8dde8f6ff61ab3
murly1980 <murly1980@gmail.com>
2019-07-04 12:24:18 +0530

    first commit

---
+++ src/org/mz/MyController.java
@@ -0,0 +1,34 @@
+package org.mz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/")
+	public String show(@ModelAttribute("user") User xyz) {
+		//model.addAttribute(new User());
+		System.out.println("show");
+		return "index";
+	}
+	
+	@RequestMapping("/test")
+	public String testUser(Model model) {
+		List<User> users=new ArrayList<User>();
+		users.add(new User("kk"));
+		users.add(new User("MK"));
+		users.add(new User("GK"));
+		model.addAttribute("users",users);
+		model.addAttribute("key","testing value");
+		//model.addAttribute(new User());
+		System.out.println("show");
+		return "user";
+	}
+
+}
---
+++ src/org/mz/User.java
@@ -0,0 +1,23 @@
+package org.mz;
+
+public class User {
+	
+	private String firstName;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public User(String firstName) {
+		this.firstName=firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
